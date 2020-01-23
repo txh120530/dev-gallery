88343707845ec4945feac677fe4b8b6a54a84a21
txh <txh120530@example.com>
2020-01-23 11:13:50 -0600

    Updated nav

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+
 
 const Navbar = props => {
   return (
+    <Router>
     <nav className="navbar">
       <ul>
-        <li><a href="profiles.html">Profiles</a></li>
-        <li><a href="gallery.html">Gallery</a> <ul><li><a href="gallery/buttons.html">Buttons</a></li></ul></li>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li><Link to="/profiles">Profiles</Link></li>
+        <li><Link to="/gallery">Gallery</Link> <ul><li><Link to="/gallery/buttons">Buttons</Link></li></ul></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
+    </Router>
   )
 }
 
